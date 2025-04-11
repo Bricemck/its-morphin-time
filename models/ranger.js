27514768a2c92628873d7d0b748cab75cbdbac2c
Brice McKeel <Bricemck@gmail.com>
2025-04-11 17:50:49 -0400

    added schema

---
+++ models/ranger.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const rangerSchema = mongoose.Schema({
+    rangerID: { type: Number, required: true},
+    name: { type: String, required: true },
+    fullName: { type: String, required: false },
+    zord: { type: Array, required: true },
+    gender: { type: String, required: true },
+    season: { type: String, required: true },
+    color: { type: String, required: true },
+    homeworld: { type: String, required: true },
+    firstAp: { type: String, required: true },
+    lastAp: { type: String, required: true },
+    numberOfAp: { type: Number, required: true },
+    actor: { type: String, required: true }
+})
+
+const Ranger = mongoose.model("Ranger", rangerSchema);
+
+module.exports = Ranger;
