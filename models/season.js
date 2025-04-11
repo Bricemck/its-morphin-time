8165c470f235e4843a06f0bfeed1fbe309ef5e61
Shawnade <deshawnagalloway@yahoo.com>
2025-04-11 18:01:08 -0400

    added season Schema

---
+++ models/season.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+
+
+const seasonSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    sentaiName: { type: String, required: true },
+    airingYear: { type: Number, required: true },
+    seasonNumber: { type: Number, required: true },
+    numberOfEpisodes: { type: Number, required: true },
+    firstEpisode: { type: String, required: true },
+    lastEpisode: { type: String, required: true },
+    theme: { type: String, required: true },
+    producer: { type: String, required: true },
+    rangers: { type: Array, required: true }
+});
+
+const Season = mongoose.model("Season", seasonSchema);
+module.exports = Season;
