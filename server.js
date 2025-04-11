155a1ae7e7888c443446755a6246f2e42cbc135a
Shawnade <deshawnagalloway@yahoo.com>
2025-04-11 18:19:06 -0400

    required season in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Season = require('[./models/season.js]');
 
 mongoose.connect(process.env.MONGODB_URI);
 
