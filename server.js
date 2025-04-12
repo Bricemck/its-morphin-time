5760a405e0ec5662cda4399069ea99ae4ac8faaf
Brice McKeel <Bricemck@gmail.com>
2025-04-12 11:23:37 -0400

    seasons model & some routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const app = express();
 const rangerRoutes = require('./routes/rangerRoutes.js');
 const megazordRoutes = require('./routes/megazordRoutes');
+const Season = require('./models/season.js');
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -21,6 +23,22 @@ app.use(express.json());
 // Routes go here
 app.use('/rangers', rangerRoutes);
 app.use('/megazords', megazordRoutes);
+
+
+app.post('/seasons', async (req, res) => {
+  const createdSeason = await Season.create(req.body);
+  res.json(createdSeason)
+});
+
+app.get('/seasons', async (req, res) => {
+  const foundSeason = await Season.find();
+  res.json(foundSeason);
+})
+
+app.delete('/seasons/:seasonId', async (req, res) => {
+  const deletedSeason = await Season.findByIdAndDelete(req.params.seasonId);
+  res.json(deletedSeason);
+})
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
