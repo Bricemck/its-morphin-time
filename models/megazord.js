317296e75532556768aa3198abe0b3951a83597b
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-04-12 12:19:24 -0400

    coneting the models

--- models/megazord.js
+++ models/megazord.js
@@ -1,11 +1,14 @@
+// models/megazord.js
 const mongoose = require('mongoose');
 
 const megazordSchema = mongoose.Schema({
   name: { type: String, required: true },
-  pictureLink: { type: String, required: true },  // Link to a picture of the Megazord
-  pilotedBy: { type: [String], required: true },  // Rangers
-  firstAppearedInSeason: { type: String, required: true },  // The season it first appeared
-  combinedMegazord: { type: String, required: true }  // The name of the Megazord it combines into
+  pictureLink: { type: String, required: true },
+  // Reference to the Ranger model(s)
+  pilotedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ranger', required: true }],
+  // Reference to the Season model
+  firstAppearedInSeason: { type: mongoose.Schema.Types.ObjectId, ref: 'Season', required: true },
+  combinedMegazord: { type: String, required: true }
 });
 
 const Megazord = mongoose.model("Megazord", megazordSchema);
--- models/ranger.js
+++ models/ranger.js
@@ -1,20 +1,21 @@
+// models/ranger.js
 const mongoose = require('mongoose');
-
 
 const rangerSchema = mongoose.Schema({
     rangerID: { type: Number, required: true},
     name: { type: String, required: true },
     fullName: { type: String, required: false },
-    zord: { type: Array, required: true },
+    zord: { type: [String], required: true },
     gender: { type: String, required: true },
-    season: { type: String, required: true },
+    // Reference to the Season model
+    season: { type: mongoose.Schema.Types.ObjectId, ref: 'Season', required: true },
     color: { type: String, required: true },
     homeworld: { type: String, required: true },
     firstAp: { type: String, required: true },
     lastAp: { type: String, required: true },
     numberOfAp: { type: Number, required: true },
     actor: { type: String, required: true }
-})
+});
 
 const Ranger = mongoose.model("Ranger", rangerSchema);
 
